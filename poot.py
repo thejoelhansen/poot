4c92e373507c6b1499956a75457e51a2197abaa5
thejoelhansen <6098265+thejoelhansen@users.noreply.github.com>
2026-03-15 09:52:16 -0700

    Finish main poot.y, gathering all found core libraries and found cmd modules.

--- poot.py
+++ poot.py
@@ -8,8 +8,20 @@ import glob
 # Dispatch to core/dispatch
 
 def dispatch():
-     
-    # Locate /core directory
+
+    # Compare libraries found against a list of required library files
+    def file_check(required_files, found_files):
+
+        missing_files = []
+
+        for file in required_files:
+            if file not in found_files:
+                missing_files.append(file)
+        if missing_files:
+            raise FileNotFoundError(f"[poot] Missing required files: {missing_files}")
+
+    # ----- Locate core libraries directory
+    
     # Precedence is pwd ./.poot/core > $HOME/.poot/core > /usr/local/lib/poot/core (later entries override earlier ones)
     core_directories = [
         Path("/usr/local/lib/poot/core"),
@@ -18,60 +30,57 @@ def dispatch():
     ]
 
     # Find all core libraries
-    core_libraries = {}
+    libraries = {}
     
     # List all fies in all core directories 
     for core_path in core_directories:
         if core_path.exists(): 
             # Update core library dict with files as they are found
             for library_path in core_path.glob('*.py'):
-                core_libraries[library_path.stem] = str(core_path)
-  
+                libraries[library_path.stem] = str(core_path)
+
+    # ----- Locate core modules directory
+    
+    # Precedence is pwd ./.poot/modules > $HOME/.poot/modules > /usr/local/lib/poot/modules (later entries override earlier ones)
+    modules_directories = [
+        Path("/usr/local/lib/poot/modules"),
+        Path.home().joinpath(".poot/modules"),
+        Path("./.poot/modules")
+    ]
+
+    # Find all core modules
+    modules = {}
+    
+    # List all fies in all core directories 
+    for modules_path in modules_directories:
+        if modules_path.exists(): 
+            # Update core module dict with files as they are found
+            for module_file in modules_path.glob('*.py'):
+                modules[module_file.stem] = str(modules_path)
+
+    # Check found libraries and modules against required files
+
     # Required core files to ensure base app functionality
-    required_core_files = [
+    required_libraries = [
         "run",
         "dispatch"
     ]
 
-    def file_check(required_files, found_libraries):
-
-        missing_files = []
-
-        for file in required_files:
-            if file not in found_libraries:
-                missing_files.append(file)
-
-        if missing_files:
-            raise FileNotFoundError(f"[poot] Missing required core files: {missing_files}")
- 
-        return missing_files 
-
-    output = file_check(required_core_files, core_libraries)
-    print(output)
-    exit()
-!! HERE not working as expected. Fix file_check funciton, move to top, then finish module_libraries, and then run both libraries through the file check. THEN on to executing (importing) uh dispatch and probably run
-    for path in core_paths:
-        print(f"Checking {path} -> {path.exists()}") 
-        if path.exists():
-            core_dir = path 
-            break
-    else: 
-        raise FileNotFoundError(f"[poot] No Core directory found: {path}")
-
-    modules_paths = [
-        Path("./.poot/modules"),
-        Path.home().joinpath(".poot/modules"),
-        Path("/usr/local/lib/modules")
+    # !! TODO  Required modules. I haven't decided if there will be any yet.
+    required_modules = [
     ]
 
-    for path in modules_paths:
-        if path.exists():
-            modules_dir = path
-            break
-    else:
-        raise FileNotFoundError(f"[poot] No Modules directory found: {path}")
-                
-    print(core_dir, modules_dir)
+    file_check(required_libraries, libraries)
+    file_check(required_modules, modules)
 
+    return libraries, modules
+    
+# Run dispatch, catching missing file errors
+try: 
+    libraries, modules = dispatch()
+    # DEBUG
+    # print(f"Found libraries: {libraries}, Found modules: {modules}")
+except FileNotFoundError as error:
+    print(error)
+    exit(1)
 
-dispatch()
